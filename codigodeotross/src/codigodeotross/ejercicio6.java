8edf8c579bfc575172090089706bfb2fc7947605
Mauricio-Noe <163358773+Mauricio-Noe@users.noreply.github.com>
2024-05-10 20:43:06 -0600

    Add files via upload
    ejercicio6

---
+++ codigodeotross/src/codigodeotross/ejercicio6.java
@@ -0,0 +1,32 @@
+package codigodeotross;
+
+import java.util.Scanner;
+
+public class ejercicio6 {
+	
+
+
+	    public static void main(String[] args) { 
+	        int[] n = new int[20]; 
+
+	        for (int i = 0; i < 20; i++) { 
+	            n[i] = (int) (Math.random() * 381) + 20;
+	            System.out.print(n[i] + " "); 
+	        }
+
+	        System.out.println("\n¿Qué números quiere resaltar? "); 
+	        System.out.print("(1 – los múltiplos de 5, 2 – los múltiplos de 7): ");
+
+	        Scanner scanner = new Scanner(System.in); 
+	        int opcion = scanner.nextInt(); 
+	        int multiplo = (opcion == 1) ? 5 : 7; 
+	        for (int e : n) { 
+	            if (e % multiplo == 0) {
+	                System.out.print("[" + e + "] ");
+	            } else {
+	                System.out.print(e + " "); 
+	        }
+	        scanner.close();
+	    }
+	}
+
